a8ab83a3fa666e22929431ba5c58b55b4e895bc4
AllAboutCoding@Avinash <48080428+Avinash711@users.noreply.github.com>
2021-05-29 17:11:22 +0530

    v5
    v5

--- medicia/doctors/views.py
+++ medicia/doctors/views.py
@@ -7,5 +7,5 @@ def doc_show(request):
     return HttpResponse('''<h1> Doc 1: Master Avinash</h1>,
                         <h2>Doc 2: Gaurav</h2>,
                         <h3>Doc 3: Sachin</h3>,
-                        <h4>Doc 4: Mrs...Supriya </h4>,
+                        <h4>Doc 4: Mrs Supriya Singhania </h4>,
                         ''')

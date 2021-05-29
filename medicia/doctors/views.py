90169cca15f459961efaae897e0fa04ae894e244
avinash711 <avinashkrgaurav711@gmail.com>
2021-05-29 16:19:05 +0530

    Initial commit

---
+++ medicia/doctors/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def doc_show(request):
+    return HttpResponse('''<h1> Doc 1: Master Avinash</h1>,
+                        <h2>Doc 2: Gaurav</h2>,
+                        <h3>Doc 3: Sachin</h3>,
+                        <h4>Doc 4: Mrs...Supriya </h4>,
+                        ''')

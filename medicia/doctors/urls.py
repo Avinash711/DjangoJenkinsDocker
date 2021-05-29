fc1825b8e14530f4d271d59b4486bbb333f86d4b
avinash711 <avinashkrgaurav711@gmail.com>
2021-05-29 17:25:48 +0530

    v6

--- medicia/doctors/urls.py
+++ medicia/doctors/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('docs/', views.doc_show),
+    path('assistants/', views.asst_show),
 ]
--- medicia/doctors/views.py
+++ medicia/doctors/views.py
@@ -9,3 +9,10 @@ def doc_show(request):
                         <h3>Doc 3: Sachin</h3>,
                         <h4>Doc 4: Mrs...Supriya </h4>,
                         ''')
+
+def asst_show(request):
+    return HttpResponse('''<h1> Doc 1: Gaurav Kumar</h1>,
+                        <h2>Doc 2: Akg Company</h2>,
+                        <h3>Doc 3: Srivastava</h3>,
+                        <h4>Doc 4: Subham </h4>,
+                        ''')
